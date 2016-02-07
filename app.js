6ed2fda590c6bd202165afa04d537881262cac72
Karl <karlkeat@gmail.com>
2016-02-07 00:21:20 -0500

    Write basic app.js code

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
-var express = require('express');
-
-var app = express();
-
-var MongoClient = require("mongodb").MongoClient;
-var mongodbURL = "mongodb://localhost:27017/myproject";
-
-app.get('/', function(req, res) {
-	MongoClient.connect(mongodbURL, function(err, db) {
-		var collection = db.collection("users");
-
-		var iAmJson = {"key" : "value", "anotherkey" : 2} 
-		res.send("hello world");
-		collection.insert(iAmJson);
-		db.close();
-		});
-	res.send("hello world");
+<script data-main="lib/capital_one" src="lib/require-jquery.js"></script>
+$(function(){
+    require(['account'], function (account) {
+        var apikey = '0bff2c40bb1c982db456e6f2ef4bbc58';
+        accountDemo(apikey, account);
+        });
 });
 
-app.listen(3000, function() {
-	console.log("example app listening on 3000");
-});
+// Methods go here
 
 
+
+
+
+//
+
+// Main code goes here
+
+var skim_amt = 0.25;
+
+while(true)
+{
+	console.log("Welcome to CapitalOne!\n Email Address: ");
+	var customerEmail = readline();
+	console.log("\nPassword: ");
+	var customerPassword = readline();
+	var customerID = fetchID(customerEmail, customerPassword); // WRITE ME
+	var customerBalance = fetchBalance(customerID); // WRITE ME
+	while (true)
+	{
+		customerBalance -= skim_amt;
+		console.log("Your balance is")
+		console.log("What would you like to do?\n[W]ithdraw/[D]eposit/[T]ransfer\n")
+		var command = readline();
+		if(command.toUpperCase() == "W" || command.toUpperCase() == "WITHDRAW")
+		{
+			console.log("\nHow much will you be withdrawing?")
+			var amount = readline();
+			withdraw_funds(customerID, amount); // WRITE ME, withdraws and transfers
+			customerBalance -= amount + skim_amt;
+			console.log("\n$" + amount + " was withdrawn from your account." +
+									" Your new balance is $" + customerBalance);
+			break;
+		}
+		else if(command.toUpperCase() == "D" || command.toUpperCase() == "DEPOSIT")
+		{
+			break;
+		}
+		else if(command.toUpperCase() == "T" || command.toUpperCase() == "TRANSFER")
+		{
+			break;
+		}
+		else
+		{
+			console.log("\nInvalid selection.")
+		}
+	}
+	console.log("\nThank you for using CapitalOne! Would you like to do something else? [Y/N]")
+	var cont = readline();
+	if(!(cont.toUpperCase() == "Y" || cont.toUpperCase() == "YES"))
+	{
+		break;
+	}
+}
